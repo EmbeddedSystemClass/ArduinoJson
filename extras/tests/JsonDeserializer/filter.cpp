21d46a37a1049b2102ebc1c5d48593df3d93dbd3
Benoit Blanchon <git@benoitblanchon.fr>
2020-01-30 13:25:12 +0100

    Use a loop for test cases

--- extras/tests/JsonDeserializer/filter.cpp
+++ extras/tests/JsonDeserializer/filter.cpp
@@ -5,195 +5,282 @@
 #include <ArduinoJson.h>
 #include <catch.hpp>
 
-static void checkJsonFilterSuccess(std::string input, std::string filter_json,
-                                   std::string expected_json) {
-  DynamicJsonDocument filter(256);
-  DynamicJsonDocument doc(256);
-
-  deserializeJson(filter, filter_json);
-
-  DeserializationError err = deserializeJson(
-      doc, input.c_str(), DeserializationOption::Filter(filter));
-
-  REQUIRE(err == DeserializationError::Ok);
-  REQUIRE(doc.as<std::string>() == expected_json);
-}
-
-static void checkJsonFilterError(std::string input, std::string filter_json,
-                                 DeserializationError expected_error) {
-  DynamicJsonDocument filter(256);
-  DynamicJsonDocument doc(256);
-
-  CAPTURE(input);
-  CAPTURE(filter_json);
-
-  deserializeJson(filter, filter_json);
-
-  DeserializationError err = deserializeJson(
-      doc, input.c_str(), DeserializationOption::Filter(filter));
-
-  REQUIRE(err == expected_error);
-}
-
 TEST_CASE("Filtering") {
-  SECTION("empty") {
-    checkJsonFilterSuccess("{\"hello\":\"world\"}", "null", "null");
-  }
+  struct TestCase {
+    std::string input;
+    std::string filter;
+    DeserializationError error;
+    std::string output;
+  };
 
-  SECTION("false") {
-    checkJsonFilterSuccess("{\"hello\":\"world\"}", "false", "null");
-  }
-
-  SECTION("true") {
-    checkJsonFilterSuccess("{\"hello\":\"world\"}", "true",
-                           "{\"hello\":\"world\"}");
-  }
-
-  SECTION("{}") {
-    checkJsonFilterSuccess("{\"hello\":\"world\"}", "{}", "{}");
-  }
-
-  SECTION("{\"key\":true}") {
-    SECTION("can skip an integer") {
-      checkJsonFilterSuccess("{\"an_integer\":666,answer:42}",
-                             "{\"answer\":true}", "{\"answer\":42}");
+  // clang-format off
+  TestCase testCases[] = {
+    {
+      "{\"hello\":\"world\"}",   // 1. input
+      "null",                    // 2. filter
+      DeserializationError::Ok,  // 3. error
+      "null"                     // 4. output
+    },
+    {
+      "{\"hello\":\"world\"}",
+      "false",
+      DeserializationError::Ok,
+      "null"
+    },
+    {
+      "{\"hello\":\"world\"}",
+      "true",
+      DeserializationError::Ok,
+      "{\"hello\":\"world\"}"
+    },
+    {
+      "{\"hello\":\"world\"}",
+      "{}",
+      DeserializationError::Ok,
+      "{}"
+    }, 
+    {
+      // skip an integer
+      "{\"an_integer\":666,answer:42}",
+      "{\"answer\":true}",
+      DeserializationError::Ok,
+      "{\"answer\":42}"
+    }, 
+    {
+      // skip a float
+      "{\"a_float\":12.34e-6,answer:42}",
+      "{\"answer\":true}",
+      DeserializationError::Ok,
+      "{\"answer\":42}"
+    },
+    {
+      // can skip a boolean
+      "{\"a_bool\":false,answer:42}",
+      "{\"answer\":true}",
+      DeserializationError::Ok,
+      "{\"answer\":42}"
+    },
+    {
+      // can skip a double-quoted string
+      "{\"a_double_quoted_string\":\"hello\",answer:42}",
+      "{\"answer\":true}",
+      DeserializationError::Ok,
+      "{\"answer\":42}"
+    },
+    {
+      // can skip a single-quoted string
+      "{\"a_single_quoted_string\":'hello',answer:42}",
+      "{\"answer\":true}",
+      DeserializationError::Ok,
+      "{\"answer\":42}"
+    },
+    {
+      // can skip an empty array
+      "{\"an_empty_array\":[],answer:42}",
+      "{\"answer\":true}",
+      DeserializationError::Ok,
+      "{\"answer\":42}"
+    },
+    {
+      // can skip an empty array with spaces in it
+      "{\"an_empty_array\":[\t],answer:42}",
+      "{\"answer\":true}",
+      DeserializationError::Ok,
+      "{\"answer\":42}"
+    },
+    {
+      // can skip an array
+      "{\"an_array\":[1,2,3],answer:42}",
+      "{\"answer\":true}",
+      DeserializationError::Ok,
+      "{\"answer\":42}"
+    },
+    {
+      // can skip an array with spaces in it
+      "{\"an_array\": [ 1 , 2 , 3 ] ,answer:42}",
+      "{\"answer\":true}",
+      DeserializationError::Ok,
+      "{\"answer\":42}"
+    },
+    {
+      // can skip an empty object
+      "{\"an_empty_object\":{},answer:42}",
+      "{\"answer\":true}",
+      DeserializationError::Ok,
+      "{\"answer\":42}"
+    },
+    {
+      // can skip an empty object with spaces in it
+      "{\"an_empty_object\":{    },answer:42}",
+      "{\"answer\":true}",
+      DeserializationError::Ok,
+      "{\"answer\":42}"
+    },
+    {
+      // can skip an object
+      "{\"an_object\":{a:1,'b':2,\"c\":3},answer:42}",
+      "{\"answer\":true}",
+      DeserializationError::Ok,
+      "{\"answer\":42}"
+    },
+    {
+      // skip an object with spaces in it
+      "{\"an_object\" : { a : 1 , 'b' : 2 , \"c\" : 3 } ,answer:42}",
+      "{\"answer\":true}",
+      DeserializationError::Ok,
+      "{\"answer\":42}"
+    },
+    {
+      "{\"an_integer\": 0,\"answer\":{\"type\":\"int\",\"value\":42}}",
+      "{\"answer\":{\"value\":true}}",
+      DeserializationError::Ok,
+      "{\"answer\":{\"value\":42}}"
+    },
+    {
+      "[1,2,3]",
+      "[false,true]",
+      DeserializationError::Ok,
+      "[2]"
+    },
+    {
+      "[1,[2.1,2.2,2.3],3]",
+      "[false,[false, true]]",
+      DeserializationError::Ok,
+      "[[2.2]]"
+    },
+    {
+      "[',2,3]",
+      "[false,true]",
+      DeserializationError::IncompleteInput,
+      "[]"
+    },
+    {
+      "[\",2,3]",
+      "[false,true]",
+      DeserializationError::IncompleteInput,
+      "[]"      
+    },
+    {
+      // ignore errors in skipped value
+      "[!,2,\\]",
+      "[false,true]",
+      DeserializationError::Ok,
+      "[2]"
+    },
+    {
+      // detect incomplete string event if it's skipped
+      "\"ABC",
+      "false",
+      DeserializationError::IncompleteInput,
+      "null"
+    },
+    {
+      // detect incomplete string event if it's skipped
+      "'ABC",
+      "false",
+      DeserializationError::IncompleteInput,
+      "null"
+    },
+    {
+      // handle escaped quotes
+      "'A\\'BC'",
+      "false",
+      DeserializationError::Ok,
+      "null"
+    },
+    {
+      // handle escaped quotes
+      "\"A\\\"BC\"",
+      "false",
+      DeserializationError::Ok,
+      "null"
+    },
+    {
+      // detect incomplete string in presence of escaped quotes
+      "'A\\'BC",
+      "false",
+      DeserializationError::IncompleteInput,
+      "null"
+    },
+    {
+      // detect incomplete string in presence of escaped quotes
+      "\"A\\\"BC",
+      "false",
+      DeserializationError::IncompleteInput,
+      "null"
+    },
+    {
+      // skip empty array
+      "[]",
+      "false",
+      DeserializationError::Ok,
+      "null"
+    },
+    {
+      // skip empty array with spaces
+      " [ ] ",
+      "false",
+      DeserializationError::Ok,
+      "null"
+    },
+    {
+      // bubble up element error even if array is skipped 
+      "[1,'2,3]",
+      "false",
+      DeserializationError::IncompleteInput,
+      "null"
+    },
+    {
+      // bubble up member error even if object is skipped 
+      "{'hello':'worl}",
+      "false",
+      DeserializationError::IncompleteInput,
+      "null"
+    },
+    {
+      // bubble up colon error even if object is skipped 
+      "{'hello','world'}",
+      "false",
+      DeserializationError::InvalidInput,
+      "null"
+    },
+    {
+      // bubble up key error even if object is skipped 
+      "{'hello:1}",
+      "false",
+      DeserializationError::IncompleteInput,
+      "null"
+    },
+    {
+      // ignore invalid value in skipped object
+      "{'hello':!}",
+      "false", 
+      DeserializationError::Ok,
+      "null"
+    },
+    {
+      // ignore invalid value in skipped object
+      "{'hello':\\}",
+      "false", 
+      DeserializationError::Ok,
+      "null"
     }
+  };  // clang-format on
 
-    SECTION("can skip a float") {
-      checkJsonFilterSuccess("{\"a_float\":12.34e-6,answer:42}",
-                             "{\"answer\":true}", "{\"answer\":42}");
-    }
+  for (size_t i = 0; i < sizeof(testCases) / sizeof(testCases[0]); i++) {
+    CAPTURE(i);
 
-    SECTION("can skip a boolean") {
-      checkJsonFilterSuccess("{\"a_bool\":false,answer:42}",
-                             "{\"answer\":true}", "{\"answer\":42}");
-    }
+    DynamicJsonDocument filter(256);
+    DynamicJsonDocument doc(256);
+    TestCase& tc = testCases[i];
 
-    SECTION("can skip a double-quoted string") {
-      checkJsonFilterSuccess("{\"a_double_quoted_string\":\"hello\",answer:42}",
-                             "{\"answer\":true}", "{\"answer\":42}");
-    }
+    CAPTURE(tc.filter);
+    REQUIRE(deserializeJson(filter, tc.filter) == DeserializationError::Ok);
 
-    SECTION("can skip a single-quoted string") {
-      checkJsonFilterSuccess("{\"a_single_quoted_string\":'hello',answer:42}",
-                             "{\"answer\":true}", "{\"answer\":42}");
-    }
+    CAPTURE(tc.input);
+    CHECK(deserializeJson(doc, tc.input.c_str(),
+                          DeserializationOption::Filter(filter)) == tc.error);
 
-    SECTION("can skip an empty array") {
-      checkJsonFilterSuccess("{\"an_empty_array\":[],answer:42}",
-                             "{\"answer\":true}", "{\"answer\":42}");
-    }
-
-    SECTION("can skip an empty array with spaces in it") {
-      checkJsonFilterSuccess("{\"an_empty_array\":[\t],answer:42}",
-                             "{\"answer\":true}", "{\"answer\":42}");
-    }
-
-    SECTION("can skip an array") {
-      checkJsonFilterSuccess("{\"an_array\":[1,2,3],answer:42}",
-                             "{\"answer\":true}", "{\"answer\":42}");
-    }
-
-    SECTION("can skip an array with spaces in it") {
-      checkJsonFilterSuccess("{\"an_array\": [ 1 , 2 , 3 ] ,answer:42}",
-                             "{\"answer\":true}", "{\"answer\":42}");
-    }
-
-    SECTION("can skip an empty object") {
-      checkJsonFilterSuccess("{\"an_empty_object\":{},answer:42}",
-                             "{\"answer\":true}", "{\"answer\":42}");
-    }
-
-    SECTION("can skip an empty object with spaces in it") {
-      checkJsonFilterSuccess("{\"an_empty_object\":{    },answer:42}",
-                             "{\"answer\":true}", "{\"answer\":42}");
-    }
-
-    SECTION("can skip an object") {
-      checkJsonFilterSuccess(
-          "{\"an_empty_object\":{a:1,'b':2,\"c\":3},answer:42}",
-          "{\"answer\":true}", "{\"answer\":42}");
-    }
-
-    SECTION("can skip an object with spaces in it") {
-      checkJsonFilterSuccess(
-          "{\"an_empty_object\" : { a : 1 , 'b' : 2 , \"c\" : 3 } ,answer:42}",
-          "{\"answer\":true}", "{\"answer\":42}");
-    }
-  }
-
-  SECTION("{\"level1\":{\"level2\":true}}") {
-    checkJsonFilterSuccess(
-        "{\"an_integer\": 0,\"answer\":{\"type\":\"int\",\"value\":42}}",
-        "{\"answer\":{\"value\":true}}", "{\"answer\":{\"value\":42}}");
-  }
-
-  SECTION("[false,true]") {
-    checkJsonFilterSuccess("[1,2,3]", "[false,true]", "[2]");
-  }
-
-  SECTION("[false,[false, true]]") {
-    checkJsonFilterSuccess("[1,[2.1,2.2,2.3],3]", "[false,[false, true]]",
-                           "[[2.2]]");
-  }
-
-  SECTION("Array: bubble up error in skipped value") {
-    checkJsonFilterError("[',2,3]", "[false,true]",
-                         DeserializationError::IncompleteInput);
-    checkJsonFilterError("[\",2,3]", "[false,true]",
-                         DeserializationError::IncompleteInput);
-  }
-
-  SECTION("Array: ignore error in skipped value") {
-    checkJsonFilterSuccess("[!,2,\\]", "[false,true]", "[2]");
-  }
-
-  SECTION("Detect incomplete double quoted string") {
-    checkJsonFilterError("\"ABC", "false",
-                         DeserializationError::IncompleteInput);
-  }
-
-  SECTION("Detect incomplete sign quoted string") {
-    checkJsonFilterError("'ABC", "false",
-                         DeserializationError::IncompleteInput);
-  }
-
-  SECTION("Handle escaped quotes") {
-    checkJsonFilterError("'A\\'BC'", "false", DeserializationError::Ok);
-    checkJsonFilterError("\"A\\\"BC\"", "false", DeserializationError::Ok);
-    checkJsonFilterError("'A\\'BC", "false",
-                         DeserializationError::IncompleteInput);
-    checkJsonFilterError("\"A\\\"BC", "false",
-                         DeserializationError::IncompleteInput);
-  }
-
-  SECTION("Skip empty array") {
-    checkJsonFilterSuccess("[]", "false", "null");
-    checkJsonFilterSuccess(" [ ] ", "false", "null");
-  }
-
-  SECTION("Bubble up errors in skipped array") {
-    checkJsonFilterError("[1,'2,3]", "false",
-                         DeserializationError::IncompleteInput);
-  }
-
-  SECTION("Bubble up value error in skipped object") {
-    checkJsonFilterError("{'hello':'worl}", "false",
-                         DeserializationError::IncompleteInput);
-  }
-
-  SECTION("Bubble up colon error in skipped object") {
-    checkJsonFilterError("{'hello','world'}", "false",
-                         DeserializationError::InvalidInput);
-  }
-
-  SECTION("Bubble up key errors in skipped object") {
-    checkJsonFilterError("{'hello:1}", "false",
-                         DeserializationError::IncompleteInput);
-  }
-
-  SECTION("Ignores invalid value in skipped object") {
-    checkJsonFilterSuccess("{'hello':!}", "false", "null");
-    checkJsonFilterSuccess("{'hello':\\}", "false", "null");
+    CHECK(doc.as<std::string>() == tc.output);
   }
 }
